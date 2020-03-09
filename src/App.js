a2c72050ae07cf9470a4c14f809f978096178e3c
sifatMoonjerin <moonjerin.sifat@gmail.com>
2020-03-09 11:50:20 +0600

    Form component added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import Form from './components/Form';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const APP_ID = `3a8d5f7e`;
+const APP_KEY = `4f9d16cc15ec1acf30530217c75ee197`;
+
+
+class App extends React.Component{
+
+  getRecipe = async (event) => {
+    event.preventDefault();
+    const search = event.target.elements.searchRecipe.value;
+    event.target.elements.searchRecipe.value = "";
+
+    const url = `https://api.edamam.com/search?q=${search}&app_id=${APP_ID}&app_key=${APP_KEY}`
+
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data.hits); 
+
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Form getRecipe={this.getRecipe}/>
+      </div>
+    );
+  }
 }
 
 export default App;
